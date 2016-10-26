223156e6ad968ab433761cf0bd60a45e2afbcb95
Matthew House <matthewhouse@MattBook.local>
2016-10-26 15:56:54 +0100

    lazy commit message is lazy

--- house-ikettle-home-assistant.py
+++ house-ikettle-home-assistant.py
@@ -47,5 +47,6 @@ def button_code(button):
     return (SET_STRING + button + '\n').encode()
 
 def press_button_on(call):
+    _LOGGER.log(host)
     s = initiate(host)
     s.send( button_code(BUTTON_ON) )
