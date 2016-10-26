2eaff538cc5d7e1956e3007f7c8c4b0885904e42
Matthew House <matthewhouse@MattBook.local>
2016-10-26 12:37:03 +0100

    lazy commit message is lazy

--- house-ikettle-home-assistant.py
+++ house-ikettle-home-assistant.py
@@ -5,13 +5,11 @@ _LOGGER = logging.getLogger(__name__)
 DOMAIN = 'ikettle'
 DEPENDENCIES = []
 
-DEFAULT_HOST = 127.0.0.1
-
 def setup(hass, config):
     # Get the host from the configuration. Use DEFAULT_TEXT if no name is provided
-    host = config[DOMAIN].get('host', DEFAULT_HOST)
+    # host = config[DOMAIN].get('text', DEFAULT_HOST)
 
     # States are set in the format DOMAIN.OBJECT_ID
-    hass.states.set('ikettle.iKettle', str(host))
+    hass.states.set('ikettle.iKettle', 'test')
 
     return True
