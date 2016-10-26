4325329661c3a3f3ba9c33467da4fb2f441142f9
h0use <matt@matthewhou.se>
2016-10-26 19:32:19 +0100

    making object-y

--- house-ikettle-home-assistant.py
+++ house-ikettle-home-assistant.py
@@ -7,45 +7,32 @@ _LOGGER = logging.getLogger(__name__)
 DOMAIN = 'ikettle'
 DEPENDENCIES = []
 
-TCP_PORT = 2000
-BUFFER_SIZE = 10
-INITIATE = b"HELLOKETTLE\n"
-
-SET_STRING = 'set sys output 0x'
-
-BUTTON_100 = '80' # Select 100C button
-BUTTON_95 = '2' # Select 95C button
-BUTTON_80 = '4000' # Select 80C button
-BUTTON_65 = '200' # Select 65C button
-BUTTON_WARM = '8' # Select Warm button
-BUTTON_WARM_5 = '8005' # Warm option is 5 mins
-BUTTON_ON = '4' # Select On button
-BUTTON_OFF = '0' # Turn off
-
 def setup(hass, config):
     # Get the host from the configuration. Use DEFAULT_TEXT if no name is provided
     host = config[DOMAIN].get('host', '127.0.0.1')
+
+    ikettle = iKettle()
 
     # States are set in the format DOMAIN.OBJECT_ID
     hass.states.set('ikettle.iKettle', host)
 
     def press_button_on(call):
-        send_message(host, button_code(BUTTON_ON))
+        ikettle.press_button_on()
 
     def press_button_off(call):
-        send_message(host, button_code(BUTTON_OFF))
+        ikettle.press_button_off()
 
     def press_button_100(call):
-        send_message(host, button_code(BUTTON_100))
+        ikettle.press_button_100()
 
     def press_button_95(call):
-        send_message(host, button_code(BUTTON_95))
+        ikettle.press_button_95()
 
     def press_button_80(call):
-        send_message(host, button_code(BUTTON_80))
+        ikettle.press_button_80()
 
     def press_button_65(call):
-        send_message(host, button_code(BUTTON_65))
+        ikettle.press_button_65()
 
     hass.services.register(DOMAIN, 'press_button_on', press_button_on)
     hass.services.register(DOMAIN, 'press_button_off', press_button_off)
@@ -56,18 +43,59 @@ def setup(hass, config):
 
     return True
 
-def initiate(host):
-    # Open a connection to the kettle
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((host, TCP_PORT))
-    s.send(INITIATE)
+class iKettle():
+    TCP_PORT = 2000
+    BUFFER_SIZE = 10
+    INITIATE = b"HELLOKETTLE\n"
 
-    return s
+    SET_STRING = 'set sys output 0x'
 
-def button_code(button):
-    return (SET_STRING + button + '\n').encode()
+    BUTTON_100 = '80' # Select 100C button
+    BUTTON_95 = '2' # Select 95C button
+    BUTTON_80 = '4000' # Select 80C button
+    BUTTON_65 = '200' # Select 65C button
+    BUTTON_WARM = '8' # Select Warm button
+    BUTTON_WARM_5 = '8005' # Warm option is 5 mins
+    BUTTON_ON = '4' # Select On button
+    BUTTON_OFF = '0' # Turn off
 
-def send_message(host, message):
-    s = initiate(host)
-    s.send(message)
-    s.close()
+    host = '127.0.0.1'
+
+    def __init__(self, host):
+        self.host = host
+
+    def _initiate(self):
+        import time
+        import socket
+
+        # Open a connection to the kettle
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((self.host, TCP_PORT))
+        s.send(INITIATE)
+        return s
+
+    def button_code(button):
+        return (SET_STRING + button + '\n').encode()
+
+    def send_message(host, message):
+        s = initiate(host)
+        s.send(message)
+        s.close()
+
+    def press_button_on(self):
+        send_message(self.host, button_code(BUTTON_ON))
+
+    def press_button_off(self):
+        send_message(self.host, button_code(BUTTON_OFF))
+
+    def press_button_100(self):
+        send_message(self.host, button_code(BUTTON_100))
+
+    def press_button_95(self):
+        send_message(self.host, button_code(BUTTON_95))
+
+    def press_button_80(self):
+        send_message(self.host, button_code(BUTTON_80))
+
+    def press_button_65(self):
+        send_message(self.host, button_code(BUTTON_65))
