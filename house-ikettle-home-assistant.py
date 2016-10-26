96dc670b1d50405322d27a8e8d5f4acffcf0e870
Matthew House <matthewhouse@MattBook.local>
2016-10-26 12:14:21 +0100

    lazy comment is lazy

--- house-ikettle-home-assistant.py
+++ house-ikettle-home-assistant.py
@@ -1,1 +1,18 @@
-hello
+import logging
+
+_LOGGER = logging.getLogger(__name__)
+
+DOMAIN = 'ikettle'
+DEPENDENCIES = []
+
+CONF_TEXT = 'text'
+DEFAULT_TEXT = 'no text'
+
+def setup(hass, config):
+    # Get the text from the configuration. Use DEFAULT_TEXT if no name is provided
+    text = config[DOMAIN].get(CONF_TEXT, DEFAULT_TEXT)
+
+    # States are set in the format DOMAIN.OBJECT_ID
+    hass.states.set('ikettle.iKettle', text)
+
+    return true
