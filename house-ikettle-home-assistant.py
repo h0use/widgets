c83587e1dfc45b4615039a41ae191815385adc49
h0use <matt@matthewhou.se>
2016-10-26 19:48:38 +0100

    still trying to fix

--- house-ikettle-home-assistant.py
+++ house-ikettle-home-assistant.py
@@ -11,7 +11,7 @@ def setup(hass, config):
     # Get the host from the configuration. Use DEFAULT_TEXT if no name is provided
     host = config[DOMAIN].get('host', '127.0.0.1')
 
-    ikettle = iKettle(host)
+    ikettle = iKettle()
 
     # States are set in the format DOMAIN.OBJECT_ID
     hass.states.set('ikettle.iKettle', host)
