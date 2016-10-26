af51c02c1b5e3372dc5712e395ae7e76126f7f1d
h0use <matt@matthewhou.se>
2016-10-26 19:19:09 +0100

    dirty script to grab your plex token

---
+++ get-plex-token.py
@@ -0,0 +1,18 @@
+import httplib, urllib, base64
+username = ""
+password = ""
+base64string = base64.encodestring('%s:%s' % (username, password)).replace('\n', '')
+txdata = ""
+
+headers={'Authorization': "Basic %s" % base64string,
+                'X-Plex-Client-Identifier': "Test script",
+                'X-Plex-Product': "Test script 356546545",
+                'X-Plex-Version': "0.001"}
+
+conn = httplib.HTTPSConnection("plex.tv")
+conn.request("POST","/users/sign_in.json",txdata,headers)
+response = conn.getresponse()
+print response.status, response.reason
+data = response.read()
+print str(data)
+conn.close()
