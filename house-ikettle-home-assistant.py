83260b336d6f22ade51a59beebe1602373d98b35
Matthew House <matthewhouse@MattBook.local>
2016-10-26 14:30:28 +0100

    lazy commit message is lazy

--- house-ikettle-home-assistant.py
+++ house-ikettle-home-assistant.py
@@ -11,14 +11,16 @@ TCP_PORT = 2000
 BUFFER_SIZE = 10
 INITIATE = b"HELLOKETTLE\n"
 
-BUTTON_100 = b'set sys output 0x80\n' # Select 100C button
-BUTTON_95 = b'set sys output 0x2\n' # Select 95C button
-BUTTON_80 = b'set sys output 0x4000\n' # Select 80C button
-BUTTON_65 = b'set sys output 0x200\n' # Select 65C button
-BUTTON_WARM = b'set sys output 0x8\n' # Select Warm button
-BUTTON_WARM_5 = b'set sys output 0x8005\n' # Warm option is 5 mins
-BUTTON_ON = b'set sys output 0x4\n' # Select On button
-BUTTON_OFF = b'set sys output 0x0\n' # Turn off
+SET_STRING = 'set sys output 0x'
+
+BUTTON_100 = '80' # Select 100C button
+BUTTON_95 = '2' # Select 95C button
+BUTTON_80 = '4000' # Select 80C button
+BUTTON_65 = '200' # Select 65C button
+BUTTON_WARM = '8' # Select Warm button
+BUTTON_WARM_5 = '8005' # Warm option is 5 mins
+BUTTON_ON = '4' # Select On button
+BUTTON_OFF = '0' # Turn off
 
 def setup(hass, config):
     # Get the host from the configuration. Use DEFAULT_TEXT if no name is provided
@@ -27,11 +29,11 @@ def setup(hass, config):
     s = initiate(host)
 
     # Send some commands to prove this works
-    s.send(BUTTON_ON)
+    s.send( (SET_STRING + BUTTON_ON + '\n').encode() )
     time.sleep(1)
-    s.send(BUTTON_95)
+    s.send( (SET_STRING + BUTTON_95 + '\n').encode() )
     time.sleep(1)
-    s.send(BUTTON_OFF)
+    s.send( (SET_STRING + BUTTON_OFF + '\n').encode() )
 
     # States are set in the format DOMAIN.OBJECT_ID
     hass.states.set('ikettle.iKettle', host)
