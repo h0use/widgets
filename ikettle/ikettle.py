126c666e062e2c63a2df2bd2dc95c5314c77a73e
Matthew House <matthewhouse@MattBook.local>
2016-11-02 20:48:29 +0000

    turning it into a switch yo

--- ikettle/ikettle.py
+++ ikettle/ikettle.py
@@ -9,8 +9,6 @@ import homeassistant.helpers.config_validation as cv
 
 CONF_IP_ADDRESS = 'ip_address'
 
-DEFAULT_NAME = 'iKettle'
-
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_IP_ADDRESS): cv.string
 })
@@ -18,17 +16,17 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 def setup_platform(hass, config, add_devices, discover_info=None):
     ip_address = config.get(CONF_IP_ADDRESS)
 
-    add_devices([iKettle(hass, ip_address)])
+    add_devices([iKettleOn(hass, ip_address)])
 
-class iKettle(SwitchDevice):
+class iKettleOn(SwitchDevice):
 
     def __init__(self, hass, ip_address):
-        import ikettle_api
+        from ikettle_api import iKettle
 
         self._hass = hass
         self._ip_address = ip_address
         self._state = False
-        self._ikettle = ikettle_api.iKettle(ip_address)
+        self._ikettle = iKettle(ip_address)
 
     @property
     def is_on(self):
