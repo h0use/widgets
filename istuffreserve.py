5930c1eea9b8ec3676e11909fcd073ea6b529ef0
Matthew House <matthewhouse@Matthews-iMac.local>
2017-11-06 22:28:12 +0000

    Added pushbullet and some other doodads

--- istuffreserve.py
+++ istuffreserve.py
@@ -1,16 +1,35 @@
-import json, requests, urlparse, urllib, webbrowser, time
+import json, requests, urlparse, urllib, webbrowser, datetime, time, sys
+from lxml import html
+from pushbullet import Pushbullet
+
 
 ''' Dr House's Crappy iThingymajig Stock Finder '''
 
 # Seconds to wait between checking
 waitTime = 10
 
+# Whether to open the page anyway
+forceOpen = False
+
+# Whether to keep running indefinitely
+runForever = False
+
+# Whether to ping you
+pushBullet = True
+pbAPIKey = ''
+
+# Whether to open a local browser or not
+openBrowser = True
+
 # The URL to pull availability JSON from
 # availabilityURL = 'https://reserve.cdn-apple.com/GB/en_GB/reserve/iPhone/availability.json'
 availabilityURL = 'https://reserve-prime.apple.com/GB/en_GB/reserve/iPhoneX/availability.json'
 # The URL to send the browser to if it finds stock
 # reserveURL = 'https://reserve.cdn-apple.com/GB/en_GB/reserve/iPhone/availability?channel=1&iPP=E'
-reserveURL = 'https://reserve-prime.apple.com/GB/en_GB/reserve/iPhoneX/availability?channel=1&iPP=E'
+reserveURL = 'https://reserve-prime.apple.com/GB/en_GB/reserve/iPhoneX/availability?channel=1&iPP=E&store={}&partNumber={}'
+
+# Check if the JSON has actually changed
+updated = ''
 
 # If you're behind a proxy, set it here
 proxyDict = {
@@ -23,11 +42,11 @@ chosenStores = {
   'R410': 'London, Stratford City',
   'R245': 'London, Covent Garden',
   'R092': 'London, Regent Street',
-  'R163': 'London, Brent Cross',
+  # 'R163': 'London, Brent Cross',
   'R226': 'London, White City',
-  'R659': 'London, Apple Watch at Selfridges',
-  'R227': 'Kingston upon Thames, Bentall Centre',
-  'R527': 'Watford, Watford',
+  # 'R659': 'London, Apple Watch at Selfridges',
+  # 'R227': 'Kingston upon Thames, Bentall Centre',
+  # 'R527': 'Watford, Watford',
   # 'R242': 'Grays, Lakeside',
   # 'R113': 'Greenhithe, Bluewater',
   # 'R269': 'Milton Keynes, Milton Keynes',
@@ -42,43 +61,53 @@ chosenSKUs = {
     }
 
 # Pulls the JSON from the server
-def getData():
-    resp = requests.get(url=availabilityURL) #, proxies=proxyDict)
-    return json.loads(resp.content)
+def getAvailData():
+    try:
+        resp = requests.get(url=availabilityURL) #, proxies=proxyDict)
+        return json.loads(resp.content)
+    except Exception:
+        time.sleep( waitTime )
+        return getAvailData()
+
+def pushMessage( title, url ):
+    pb = Pushbullet( pbAPIKey ) #, proxy=proxyDict)
+    push = pb.push_link( title, url )
 
 # Now for the fun bit
 foundStock = False
-while foundStock == False:
-    data = getData()
+while not foundStock or runForever:
+    availData = getAvailData()
 
-    # Check if the availability file is up yet
-    siteUp = False
-    while siteUp == False:
-        if len(data.keys()) == 0:
-            print time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime()) + ' - No data found, waiting 1 seconds before trying again'
-            time.sleep(1)
-            data = getData()
-        else:
-            siteUp = True
+    if not availData['updated'] == updated:
+        updated = availData['updated']
+        dt = datetime.datetime.fromtimestamp( float( updated )/1000 )
+        print "Last updated at: {}".format( dt.strftime('%Y-%m-%d %H:%M:%S') )
 
-    # Do the needful
-    for chosenStore in chosenStores:
-        for store in data['stores']:
-            if store == chosenStore:
-                print 'Checking ' + chosenStores[chosenStore]
-                for chosenSKU in chosenSKUs:
-                    for SKU in data['stores'][store]:
-                        if SKU == chosenSKU:
-                            print '    ' + 'Checking ' + chosenSKUs[chosenSKU]
-                            if data['stores'][store][SKU]['availability']['contract'] == False:
-                                # Awesome, we found one
-                                print '        Found ' + str( data['stores'][store][SKU]['availability'] )
-                                # Generate a URL to take you straight to it
-                                url = reserveURL + '&store=' + chosenStore + '&partNumber=' + urllib.quote_plus(chosenSKU)
-                                print url
-                                # Open that in your default browser
-                                webbrowser.open(url)
-                                # foundStock = True
+        # Do the needful
+        for chosenStore in chosenStores:
+            for store in availData['stores']:
+                if store == chosenStore:
+                    for chosenSKU in chosenSKUs:
+                        for SKU in availData['stores'][store]:
+                            if SKU == chosenSKU:
+                                if ( availData['stores'][store][SKU]['availability']['contract'] == True ) or ( availData['stores'][store][SKU]['availability']['unlocked'] == True ) or forceOpen:
+                                    # Awesome, we found one
+                                    notification = 'Found {} at {}'.format( chosenSKUs[chosenSKU], chosenStores[chosenStore]  )
+                                    # Generate a URL to take you straight to it
+                                    url = reserveURL.format( chosenStore, urllib.quote_plus(chosenSKU) )
+                                    # Notifications
+                                    print notification
+                                    if pushBullet:
+                                        pushMessage( notification, url )
+                                    if openBrowser:
+                                        webbrowser.open(url)
 
-    print time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime()) + ' - None found, waiting ' + str(waitTime) + ' seconds before trying again'
-    time.sleep(waitTime)
+                                    foundStock = True
+
+        if not foundStock:
+            print time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime()) + ' - None found, waiting ' + str(waitTime) + ' seconds before trying again'
+            time.sleep(waitTime)
+
+    else:
+        print 'Availability hasn\'t changed since last run'
+        time.sleep(waitTime)
